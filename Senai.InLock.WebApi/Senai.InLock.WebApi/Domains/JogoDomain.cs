55ae95c5cde5bae52dccfcd77651b9c0b205c88a
André Santos De Azevedo <54094513841@info.local>
2020-03-02 11:26:28 -0300

    api

---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Domains/JogoDomain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Domains
+{
+    public class JogoDomain
+    {
+
+        public int ID_JOGO { get; set; }
+        public string NOME_JOGO { get; set; }
+        public string DESCRICAO { get; set; }
+        public DateTime DATA_LANCAMENTO { get; set; }
+        public float VALOR { get; set; }
+        public int ID_ESTUDIO { get;set; }
+
+    }
+}
---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Repositories/JogoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Senai.InLock.WebApi.Domains;
+using Senai.InLock.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+        public List<JogoDomain> Listar()
+        {
+
+            List < JogoDomain > jogos = new List< JogoDomain >();
+
+            return jogos;
+
+        }
+    }
+}
